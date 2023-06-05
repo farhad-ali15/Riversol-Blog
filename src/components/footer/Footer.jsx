3752ba3820e4b6ee659f77fa6f3170909a3138f9
farhad-ali15 <farhadali15@yahoo.com>
2023-06-05 17:26:38 +0500

    Footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,12 +5,36 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>©2023 Lamamia. All rights reserved.</div>
+      <div>©2023 Riversol. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Lama Dev Facebook Account" />
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Lama Dev" />
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Riversol  Facebook Account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Riversol Instagram "
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Riversol Twitter Account"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Riversol YouTube Account"
+        />
       </div>
     </div>
   );
